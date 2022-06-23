22f0e9c875fb89fc607fa6e689417b934d17494c
Sraosha47 <sorusch.afkhami@gmail.com>
2022-06-23 11:23:53 +0200

    Added a form and PHP to insert the input into the Accounts table

--- thebookstore_admin/add_user.php
+++ thebookstore_admin/add_user.php
@@ -12,29 +12,73 @@ session_start();
     <title>The Bookstore|New Account</title>
 </head>
 <body>
+
+    <?php
+        if ( isset($_POST['fname'])  
+            && isset($_POST['lname']) 
+            && isset($_POST['email'])
+            && isset($_POST['password'])) {
+
+            // Data validation
+            if ( strlen($_POST['fname']) < 1 || strlen($_POST['password']) < 1) {
+            $_SESSION['error'] = 'Missing data';
+            header("Location: add.php");
+            return;
+            }
+
+            if ( strpos($_POST['email'],'@') === false ) {
+            $_SESSION['error'] = 'Bad data';
+            header("Location: add_user.php");
+            return;
+            }
+
+            $sql = "INSERT INTO accounts (First_Name, Last_Name, Email, 'Password', Phone, Street, Admin, Postal_FK)
+                    VALUES (:fname, :lname, :email, :password, :phone, :street, :admin, (SELECT Postal_ID FROM postal_codes WHERE CODE LIKE :pcode))";
+            $stmt = $pdo->prepare($sql);
+            $stmt->execute(array(
+            ':fname' => $_POST['fname'],
+            ':lname' => $_POST['lname'],
+            ':email' => $_POST['email'],
+            ':password' => $_POST['password'],
+            ':phone' => $_POST['phone'],
+            ':address' => $_POST['address'],
+            ':admin' => $_POST['admin'],
+            ':pcode' => $_POST['pcode']
+            ));
+            $_SESSION['success'] = 'Account Added';
+            header( 'Location: index.php' ) ;
+            return;
+        }
+
+        // Flash pattern
+        if ( isset($_SESSION['error']) ) {
+        echo '<p style="color:red">'.$_SESSION['error']."</p>\n";
+        unset($_SESSION['error']);
+        }
+    ?>
+
     <h1>New Account</h1>
     <form method="post">
-        <p>First Name:
-        <input type="text" name="fname"></p>
-        <p>Last Name:
-        <input type="text" name="lname"></p>
-        <p>Email:
-        <input type="text" name="email"></p>
-        <p>Password:
-        <input type="password" name="password"></p>
-        <p>Phone:
-        <input type="text" name="phone"></p>
-        <p>Address:
-        <input type="text" name="address"></p>
-        <p>Postal Code:
-        <input type="text" name="pcode"></p>
-        <p>Town:
-        <input type="text" name="town"></p>
-        <p>Admin:
-        <input type="radio" id="yes" name="admin" value=1>
-        <label for="yes">Yes</label>
-        <input type="radio" id="no" name="admin" value=0>
-        <label for="no">No</label></p>
+        <p><label for="fname">First Name:</label>
+        <input type="text" id="fname" name="fname"></p>
+        <p><label for="lname">Last Name:</label>
+        <input type="text" id="lname" name="lname"></p>
+        <p><label for="email">Email:</label>
+        <input type="email" id="email" name="email"></p>
+        <p><label for="password">Password:</label>
+        <input type="password" id="password" name="password"></p>
+        <p><label for="phone">Phone:</label>
+        <input type="text" id="phone" name="phone"></p>
+        <p><label for="address">Address:</label>
+        <input type="text" id="address" name="address"></p>
+        <p><label for="pcode">Postal Code:</label>
+        <input type="text" id="pcode" name="pcode"></p>
+        <p><label for="admin">Admin:</label>
+        <input type="text" id="admin" name="admin"></p>
+<!--        <select id="admin" name="admin">
+            <option value="0">No</option>
+            <option value="1">Yes</option>
+        </select></p> -->
         <p><input type="submit" value="Add"/>
         <a href="add_user.php">Cancel</a></p>
     </form>
