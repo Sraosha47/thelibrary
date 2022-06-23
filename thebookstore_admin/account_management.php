93c41b7012ba4f9cfdbedca09e8a6367699a5485
Sraosha47 <sorusch.afkhami@gmail.com>
2022-06-23 13:39:33 +0200

    added a link to the add_user page

--- thebookstore_admin/account_management.php
+++ thebookstore_admin/account_management.php
@@ -55,7 +55,7 @@ session_start();
             echo('<a href="delete_user.php?Account_ID='.$row['Account_ID'].'">Delete</a>');
             echo("</td></tr>\n");
         }
-        echo('<a href="add_user.php">Add New Account</a>');
+                    echo('<a href="add_user.php">Add New Account</a>');
 
     ?>
 </body>
