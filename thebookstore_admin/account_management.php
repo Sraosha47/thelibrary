8b59ebe1a032c368f4a8e0fd25e80dcb1512f011
Sraosha47 <sorusch.afkhami@gmail.com>
2022-06-24 09:00:04 +0200

    Added Navigation, changed tableheaders to <th>, and inserted the 'add new account' link to the table

--- thebookstore_admin/account_management.php
+++ thebookstore_admin/account_management.php
@@ -12,6 +12,15 @@ session_start();
     <title>The Bookstore|Account Management</title>
 </head>
 <body>
+    <nav id="navbar" class="nav">
+    <ul class="nav-list">
+        <li><a href="account_management.php">Account Management</a></li>
+        <li><a href="library_management.php">Library</a></li>
+        <li><a href="rentals.php">Rentals</a></li>
+        <li><a href="index.php">Log Out</a></li>   
+    </ul>
+    </nav>
+
     <h1>Account Management</h1>
 
     <?php
@@ -24,38 +33,43 @@ session_start();
             unset($_SESSION['success']);
         }
         echo('<table border="1">'."\n");
-        echo "<tr><td>";
-        echo('First_Name');
-        echo("</td><td>");
-        echo('Last_Name');
-        echo("</td><td>");
-        echo('Email');
-        echo("</td><td>");
-        echo('Admin');
-        echo("</td><td>");
-        echo('Action');
-        echo("</td></tr>\n");
-        $stmt = $pdo->query("SELECT First_Name, Last_Name, Email, Admin, Account_ID FROM accounts");
+            echo "<tr><th>";
+            echo('First_Name');
+            echo("</th><th>");
+            echo('Last_Name');
+            echo("</th><th>");
+            echo('Email');
+            echo("</th><th>");
+            echo('Admin');
+            echo("</th><th>");
+            echo('Action');
+            echo("</th></tr>\n");
+            $stmt = $pdo->query("SELECT First_Name, Last_Name, Email, Admin, Account_ID FROM accounts");
 
-        while ( $row = $stmt->fetch(PDO::FETCH_ASSOC) ) {
-            echo "<tr><td>";
-            echo(htmlentities($row['First_Name']));
-            echo("</td><td>");
-            echo(htmlentities($row['Last_Name']));
-            echo("</td><td>");
-            echo(htmlentities($row['Email']));
-            echo("</td><td>");
-            if($row['Admin']) {
-                echo("Yes");
-            } else {
-                echo("No");
+            while ( $row = $stmt->fetch(PDO::FETCH_ASSOC) ) {
+                echo "<tr><td>";
+                echo(htmlentities($row['First_Name']));
+                echo("</td><td>");
+                echo(htmlentities($row['Last_Name']));
+                echo("</td><td>");
+                echo(htmlentities($row['Email']));
+                echo("</td><td>");
+                if($row['Admin']) {
+                    echo("Yes");
+                } else {
+                    echo("No");
+                }
+                echo("</td><td>");
+                echo('<a href="edit_user.php?Account_ID='.$row['Account_ID'].'">Edit</a> / ');
+                echo('<a href="delete_user.php?Account_ID='.$row['Account_ID'].'">Delete</a>');
+                echo("</td></tr>\n");
             }
-            echo("</td><td>");
-            echo('<a href="edit_user.php?Account_ID='.$row['Account_ID'].'">Edit</a> / ');
-            echo('<a href="delete_user.php?Account_ID='.$row['Account_ID'].'">Delete</a>');
+            echo( "<tr style='text-align:center'><td colspan='5'>");
+            echo('<a href="add_user.php">Add New Account</a>');
             echo("</td></tr>\n");
-        }
-                    echo('<a href="add_user.php">Add New Account</a>');
+        echo("</table>")
+
+                   
 
     ?>
 </body>
