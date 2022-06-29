6d7b03075225fdbe50374fa7d3b92210fcc3513c
Sraosha47 <sorusch.afkhami@gmail.com>
2022-06-29 08:16:25 +0200

    Integrated the Add_User form into the account_management.php

--- thebookstore_admin/account_management.php
+++ thebookstore_admin/account_management.php
@@ -64,13 +64,80 @@ session_start();
                 echo('<a href="delete_user.php?Account_ID='.$row['Account_ID'].'">Delete</a>');
                 echo("</td></tr>\n");
             }
-            echo( "<tr style='text-align:center'><td colspan='5'>");
-            echo('<a href="add_user.php">Add New Account</a>');
-            echo("</td></tr>\n");
-        echo("</table>")
+        echo("</table>");
 
-                   
+        if ( isset($_POST['fname'])  
+        && isset($_POST['lname']) 
+        && isset($_POST['email'])
+        && isset($_POST['password'])) {
+
+        // Data validation
+        if ( strlen($_POST['fname']) < 1 || strlen($_POST['password']) < 1) {
+        $_SESSION['error'] = 'Missing data';
+        header("Location: add_user.php");
+        return;
+        }
+
+        if ( strpos($_POST['email'],'@') === false ) {
+        $_SESSION['error'] = 'Bad data';
+        header("Location: add_user.php");
+        return;
+        }
+
+        $sql = "INSERT INTO accounts (First_Name, Last_Name, Email, Password, Phone, Street, Postal_FK, Admin)
+                VALUES (:fname, :lname, :email, :password, :phone, :address, (SELECT Postal_ID FROM postal_codes WHERE CODE LIKE :pcode && Town LIKE :town), :admin)";
+        $add_user = $pdo->prepare($sql);
+        $add_user->execute(array(
+        ':fname' => $_POST['fname'],
+        ':lname' => $_POST['lname'],
+        ':email' => $_POST['email'],
+        ':password' => $_POST['password'],
+        ':phone' => $_POST['phone'],
+        ':address' => $_POST['address'],
+        ':pcode' => $_POST['pcode'],
+        ':town' => $_POST['town'],
+        ':admin' => $_POST['admin']
+        ));
+        $_SESSION['success'] = 'Account Added';
+        header( 'Location: account_management.php' ) ;
+        return;
+    }
+
+        // Flash pattern
+        if ( isset($_SESSION['error']) ) {
+        echo '<p style="color:red">'.$_SESSION['error']."</p>\n";
+        unset($_SESSION['error']);
+        }
+
 
     ?>
+
+    <h2>New Account</h2>
+
+    <form method="post">
+        <p><label for="fname">First Name:</label>
+        <input type="text" id="fname" name="fname"></p>
+        <p><label for="lname">Last Name:</label>
+        <input type="text" id="lname" name="lname"></p>
+        <p><label for="email">Email:</label>
+        <input type="email" id="email" name="email"></p>
+        <p><label for="password">Password:</label>
+        <input type="password" id="password" name="password"></p>
+        <p><label for="phone">Phone:</label>
+        <input type="text" id="phone" name="phone"></p>
+        <p><label for="address">Address:</label>
+        <input type="text" id="address" name="address"></p>
+        <p><label for="pcode">Postal Code:</label>
+        <input type="text" id="pcode" name="pcode"></p>
+        <p><label for="town">Town:</label>
+        <input type="text" id="town" name="town"></p>
+        <p><label for="admin">Admin:</label>
+        <input type="radio" id="yes" name="admin" value=1>
+        <label for="yes">Yes</label>
+        <input type="radio" id="no" name="admin" value=0>
+        <label for="no">No</label></p>
+        <p><input type="submit" value="Add"/>
+        <a href="account_management.php">Cancel</a></p>
+    </form>
 </body>
 </html>
