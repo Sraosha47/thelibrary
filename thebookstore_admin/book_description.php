a6f48cfbd949ae9a33533697391d3ad8c2a2b50a
Sraosha47 <sorusch.afkhami@gmail.com>
2022-06-29 18:35:06 +0200

    Editing the Attributes of the books table works now. I decided to show the authors and genres as a table, adding a remove href. I also added dropdown menus and add buttons to add authors and genres

--- thebookstore_admin/book_description.php
+++ thebookstore_admin/book_description.php
@@ -1,63 +1,61 @@
 <?php
 require_once "pdo.php";
 session_start();
- /*if ( isset($_POST['title']) && isset($_POST['descr']) && isset($_POST['isbn'])
+
+if ( isset($_POST['title']) && isset($_POST['descr']) && isset($_POST['isbn'])
      && isset($_POST['release']) && isset($_POST['id']) ) {
 
     // Data validation
-    if ( strlen($_POST['title']) < 1 || strlen($_POST['description']) < 1) {
+    /*if ( strlen($_POST['title']) < 1 || strlen($_POST['descr']) < 1) {
         $_SESSION['error'] = 'Missing data';
-        header("Location: book_description.php?Book_ID=".$_POST['Book_ID']);
-        return;
-    }
+        header("Location: library_management.php");
+        exit;
+    }*/
 
-    $sql = "UPDATE books b SET 
-            JOIN authors_books ab
-                ON b.Book_ID = ab.Book_FK
-            JOIN authors a
-               ON a.Author_ID = ab.Author_FK
-           JOIN genres_books gb
-              ON b.Book_ID = gb.Book_FK
-            JOIN genres g
-                ON gb.Genre_FK = g.Genre_ID
-            b.Title = :title,
-            b.Description = :descr,
-            b.ISBN = :isbn, 
-            b.Release_Date = :release,
-            b.Available = :available,
-            g.Genre = :genre, 
-            WHERE Book_ID = :Book_ID"; 
-    $stmt = $pdo->prepare($sql);
-    $stmt->execute(array(
+   /* $del_genres = "DELETE FROM genres_books where Book_FK like :Book_ID;
+INSERT INTO genres_books (Genre_FK, Book_FK) VALUES (SELECT Genre_ID FROM genres where genre like :genres;   
+    ";
+    $chngGenre = $pdo->prepare($del_genres);
+    $chngGenre->execute(array(
+        ':Book_id' => $_POST['id'],
+        ':genres' => $_POST['genre']
+    ));*/
+    $sql = "UPDATE books SET 
+            Title = :title,
+            Description = :descr,
+            ISBN = :isbn, 
+            Release_Date = :release,
+            Available = :available
+            WHERE Book_ID = :id;"; 
+    $upd_book = $pdo->prepare($sql);
+    $upd_book->execute(array(
         ':title' => $_POST['title'],
         ':descr' => $_POST['descr'],
         ':isbn' => $_POST['isbn'],
         ':release' => $_POST['release'],
         ':available' => $_POST['available'],
-        ':genre' => $_POST['genre'],
-        ':author' => $_POST['author'],
-        ':Book_ID' => $_POST['id']));
+        ':id' => $_POST['id']));
     $_SESSION['success'] = 'Record updated';
-    header( 'Location: account_management.php' );
-    return;
-} */
+    header( 'Location: library_management.php' );
+    exit;
+} 
 
 // Guardian: Make sure that Book_ID is present
 if ( ! isset($_GET['Book_ID']) ) {
   $_SESSION['error'] = "Missing Book_ID";
   header('Location: library_management.php');
-  return;
+  exit;
 }
 
 $stmt = $pdo->prepare(
     "SELECT * FROM books
-    WHERE Book_ID = :ID");
+    WHERE Book_ID = :ID;");
 $stmt->execute(array(":ID" => $_GET['Book_ID']));
 $row = $stmt->fetch(PDO::FETCH_ASSOC);
 if ( $row === false ) {
     $_SESSION['error'] = 'Bad value for Book_ID';
     header( 'Location: library_management.php' ) ;
-    return;
+    exit;
 }
 
 // Flash pattern
@@ -92,7 +90,7 @@ while ($row = $Genres->fetch(PDO::FETCH_ASSOC)){
 //array containing all Authors associated with the book
 $Arr_Authors = array();
 $Authors = $pdo->prepare(
-    "SELECT concat(a.First_Name, ' ', a.Last_Name) as Author from books b
+    "SELECT ab.authors_books_ID, concat(a.First_Name, ' ', a.Last_Name) as Author from books b
     JOIN authors_books ab
         on ab.Book_FK = b.Book_ID
     Join authors a
@@ -103,7 +101,7 @@ $Authors->execute(array(":ID" => $_GET['Book_ID']));
 
 while ($row = $Authors->fetch(PDO::FETCH_ASSOC)){
     array_push($Arr_Authors, $row['Author']);
-}
+} 
 ?>
 
 <!DOCTYPE html>
@@ -112,6 +110,8 @@ while ($row = $Authors->fetch(PDO::FETCH_ASSOC)){
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+    <link rel="stylesheet" href="libstyle.css">
     <title>TB | Book Description</title>
 </head>
 <body>
@@ -125,7 +125,7 @@ while ($row = $Authors->fetch(PDO::FETCH_ASSOC)){
     </nav>
     <h1>Book Description</h1>
     <form method="post">
-        <input type="hidden" name="id" value=<?= $book ?>>
+        <input type="hidden" name="id" value="<?= $book ?>">
         <p>Title:
         <input type="text" name="title" value="<?= $title ?>"></p>
         <p>Description:
@@ -139,9 +139,9 @@ while ($row = $Authors->fetch(PDO::FETCH_ASSOC)){
         <label for="yes">Yes</label>
         <input type="radio" id="no" name="available" <?php if(!$available) {echo("checked");} ?> value=0>
         <label for="no">No</label></p>
-        <p>Genre:
+        <!--<p>Genre:
         <select name="genre" multiple>
-            <?php
+            <php 
             //turns all the genres in the table Genres to options in the select field
             $stmt = $pdo->query("SELECT Genre FROM genres");
             while ( $row = $stmt->fetch(PDO::FETCH_ASSOC)){
@@ -149,14 +149,14 @@ while ($row = $Authors->fetch(PDO::FETCH_ASSOC)){
                 if(in_array($row['Genre'], $Arr_Genres)){$selected = htmlentities(' selected');}
                 else {$selected = "";}
                 echo "<option value=" . htmlentities($row['Genre']) . $selected .  ">" . htmlentities($row['Genre']) . "</option>";
-            }
+            } 
             ?>
         </select>
         </p>
 
         <p>Author:
         <select name="author" multiple>
-            <?php
+            <php 
             //turns all the genres in the table Genres to options in the select field
             $stmt = $pdo->query("SELECT concat(First_Name, ' ', Last_Name) as Author FROM authors");
             while ( $row = $stmt->fetch(PDO::FETCH_ASSOC)){
@@ -164,9 +164,93 @@ while ($row = $Authors->fetch(PDO::FETCH_ASSOC)){
                 if(in_array($row['Author'], $Arr_Authors)){$selected = htmlentities(' selected');}
                 else {$selected = "";}
                 echo "<option value=" . htmlentities($row['Author']) . $selected .  ">" . htmlentities($row['Author']) . "</option>";
-            }
+            } 
             ?>
         </select>
+        </p>-->
+        <p><input type="submit" value="Update"/>
+        <a href="library_management.php">Cancel</a></p>
+    </form>
+
+    <?php 
+        echo('<table border="1">'."\n");
+        echo("<tr><th>");
+        echo('Authors');
+        echo("</th><th>");
+        echo("Action");
+        echo("</th></tr>");
+        foreach($Arr_Authors as $author){
+            $stmt = $pdo->query("SELECT ab.Authors_Books_ID as Entry FROM authors_books ab 
+            join authors a 
+                on a.Author_ID = ab.Author_FK 
+            join books b 
+                on b.Book_ID = ab.Book_FK 
+            where concat(a.First_Name, ' ', a.Last_Name) = '$author' AND b.ISBN = '$isbn' ");
+            $stmt->execute();
+            $result = $stmt->fetch(PDO::FETCH_ASSOC);
+            echo("<tr><td>\n");
+            echo($author);
+            echo("</td><td>");
+
+            
+            echo('<a href="remove_author.php?A_B_ID='.$result['Entry'].'">Remove</a>');
+            echo("</td></tr>");
+        }
+        echo("</table>");
+
+        echo('<table border="1">'."\n");
+        echo("<tr><th>");
+        echo('Genres');
+        echo("</th><th>");
+        echo("Action");
+        echo("</th></tr>");
+        foreach($Arr_Genres as $genre){
+            $stmt = $pdo->query("SELECT gb.Genres_Books_ID as Entry FROM genres_books gb 
+            join genres g 
+                on g.Genre_ID = gb.Genre_FK 
+            join books b 
+                on b.Book_ID = gb.Book_FK 
+            where g.Genre = '$genre' AND b.ISBN = '$isbn' ");
+            $stmt->execute();
+            $result = $stmt->fetch(PDO::FETCH_ASSOC);
+            echo("<tr><td>\n");
+            echo($genre);
+            echo("</td><td>");
+
+            
+            echo('<a href="remove_genre.php?G_B_ID='.$result['Entry'].'">Remove</a>');
+            echo("</td></tr>");
+        }
+        echo("</table>");
+    ?>
+
+    <form method="post">
+        <p>Author:
+            <select name="author">
+                <?php 
+                //turns all the genres in the table Genres to options in the select field
+                $stmt = $pdo->query("SELECT concat(First_Name, ' ', Last_Name) as Author FROM authors");
+                while ( $row = $stmt->fetch(PDO::FETCH_ASSOC)){
+                    echo "<option value=''>" . htmlentities($row['Author']) . "</option>";
+                } 
+                ?>
+            </select>
+        </p>
+        <p><input type="submit" value="Add Author"/></p>
+    </form>
+    <form method="POST">
+        <p>Genre:
+            <select name="genre">
+                <?php 
+                //turns all the genres in the table Genres to options in the select field
+                $stmt = $pdo->query("SELECT Genre FROM genres");
+                while ( $row = $stmt->fetch(PDO::FETCH_ASSOC)){
+                    echo "<option value=''>" . htmlentities($row['Genre']) . "</option>";
+                } 
+                ?>
+            </select>
+        </p>
+        <p><input type="submit" value="Add Genre"/></p>
     </form>
 
 </body>
