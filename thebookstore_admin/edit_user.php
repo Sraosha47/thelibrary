4a0ee596bf6523f3d5afe66aab626ea8dc04dce7
Sraosha47 <sorusch.afkhami@gmail.com>
2022-06-30 21:37:17 +0200

    added a table that shows all the books rented by the user and allows to declare a book as returned

--- thebookstore_admin/edit_user.php
+++ thebookstore_admin/edit_user.php
@@ -9,55 +9,84 @@ if($_SESSION['admin'] === false){
     exit;
 }
 
-
-if ( isset($_POST['fname']) && isset($_POST['lname']) && isset($_POST['email'])
-     && isset($_POST['password']) && isset($_POST['id']) ) {
-
-    // Data validation
-    if ( strlen($_POST['fname']) < 1 || strlen($_POST['password']) < 1) {
-        $_SESSION['error'] = 'Missing data';
-        header("Location: edit.php?Account_ID=".$_POST['Account_ID']);
-        exit;
-    }
-
-    if ( strpos($_POST['email'],'@') === false ) {
-        $_SESSION['error'] = 'Bad data';
-        header("Location: edit.php?Account_ID=".$_POST['Account_ID']);
-        exit;
-    }
-
-    $sql = "UPDATE accounts SET 
-            First_Name = :fname,
-            Last_Name = :lname,
-            Email = :email, 
-            Password = :password,
-            Phone = :phone,
-            Street = :address,
-            Postal_FK = (Select Postal_ID From postal_codes where Code like :code && Town like :town)
-            WHERE Account_ID = :Account_ID;";
-    $stmt = $pdo->prepare($sql);
-    $stmt->execute(array(
-        ':fname' => $_POST['fname'],
-        ':lname' => $_POST['lname'],
-        ':email' => $_POST['email'],
-        ':password' => $_POST['password'],
-        ':phone' => $_POST['phone'],
-        ':address' => $_POST['address'],
-        ':town' => $_POST['town'],
-        ':code' => $_POST['code'],
-        ':Account_ID' => $_POST['id']));
-    $_SESSION['success'] = 'Record updated';
-    header("Location: account_management.php");
+// Guardian: Make sure that Account_ID is present
+if ( ! isset($_GET['Account_ID']) ) {
+    $_SESSION['error'] = "Missing Account_ID";
+    header('Location: index.php');
     exit;
 }
 
-// Guardian: Make sure that Account_ID is present
-if ( ! isset($_GET['Account_ID']) ) {
-  $_SESSION['error'] = "Missing Account_ID";
-  header('Location: index.php');
-  exit;
+if ( isset($_POST['return']) && $_POST['rDate'] !== 0){
+    $_SESSION['error'] = ' Book already returned';
+    header( 'Location: edit_user.php?Account_ID='.$_POST['Account'].'#rentals' ) ;
+    exit;   
 }
 
+//statement returning the book
+elseif ( isset($_POST['return']) && isset($_POST['Account'])) {
+ 
+    $sql = 
+    "UPDATE books
+    SET available = 1
+    WHERE title = :title;
+    
+    UPDATE rentals
+    SET Return_Date = CURRENT_TIMESTAMP
+    WHERE Account_FK = :account AND Book_FK = :book;";
+    $stmt = $pdo->prepare($sql);
+    $stmt->execute(array(
+        ':title' => $_POST['Title'],
+        ':account' => $_POST['Account'],
+        ':book' => $_POST['Book']
+    ));
+    $_SESSION['success'] = ' Book Returned';
+    header( 'Location: edit_user.php?Account_ID='.$_POST['Account'].'#rentals' ) ;
+    exit;
+  }
+
+//statement for updates to the account
+elseif ( isset($_POST['fname']) && isset($_POST['lname']) && isset($_POST['email']) 
+&& isset($_POST['password']) && isset($_POST['id']) && isset($_POST['changes']) ) {
+
+ // Data validation
+ if ( strlen($_POST['fname']) < 1 || strlen($_POST['password']) < 1) {
+     $_SESSION['error'] = 'Missing data';
+     header("Location: edit.php?Account_ID=".$_POST['Account_ID']);
+     exit;
+ }
+
+ if ( strpos($_POST['email'],'@') === false ) {
+     $_SESSION['error'] = 'Bad data';
+     header("Location: edit.php?Account_ID=".$_POST['Account_ID']);
+     exit;
+ }
+
+ $sql = "UPDATE accounts SET 
+         First_Name = :fname,
+         Last_Name = :lname,
+         Email = :email, 
+         Password = :password,
+         Phone = :phone,
+         Street = :address,
+         Postal_FK = (Select Postal_ID From postal_codes where Code like :code && Town like :town)
+         WHERE Account_ID = :Account_ID;";
+ $stmt = $pdo->prepare($sql);
+ $stmt->execute(array(
+     ':fname' => $_POST['fname'],
+     ':lname' => $_POST['lname'],
+     ':email' => $_POST['email'],
+     ':password' => $_POST['password'],
+     ':phone' => $_POST['phone'],
+     ':address' => $_POST['address'],
+     ':town' => $_POST['town'],
+     ':code' => $_POST['code'],
+     ':Account_ID' => $_POST['id']));
+ $_SESSION['success'] = 'Record updated';
+ header("Location: edit.php?Account_ID=".$_POST['Account_ID']);
+ exit;
+}
+
+//getting all the info on the account
 $stmt = $pdo->prepare(
     "SELECT *  
     FROM accounts 
@@ -70,12 +99,6 @@ if ( $row === false ) {
     $_SESSION['error'] = 'Bad value for Account_ID';
     header( 'Location: account_management.php' ) ;
     exit;
-}
-
-// Flash pattern
-if ( isset($_SESSION['error']) ) {
-    echo '<p style="color:red">'.$_SESSION['error']."</p>\n";
-    unset($_SESSION['error']);
 }
 
 $firstname = htmlentities($row['First_Name']);
@@ -107,10 +130,19 @@ $account = $row['Account_ID'];
             <li><a href="library_management.php">Library Management</a></li>
             <li><a href="rentals.php">Rentals</a></li>
             <li><a href="index.php">Log Out</a></li>   
+            <div class="modnav"> 
+                <li><a href="#info">Account Information</a></li>
+                <li><a href="#rentals">Rentals</a></li> 
+            </div>
+
         </ul>
     </nav>
-    <section class="tables">
+    <section class="tables" id="info">
     <h1>Edit Account</h1>
+
+    <h2>Account Information</h2>
+<?php
+?>
     <form method="post">
         <input type="hidden" name="id" value="<?= $account ?>">
         <p>First Name:
@@ -129,9 +161,71 @@ $account = $row['Account_ID'];
         <input type="text" name="code" value="<?= $code ?>"></p>
         <p>Town:
         <input type="text" name="town" value="<?= $town ?>"></p>
-        <p class="buttons"><input type="submit" value="Submit Changes"/>
+        <p class="buttons"><input name="changes" type="submit" value="Submit Changes"/>
         <button type="reset">Cancel</button></p></p>
     </form>
     </section>
+
+    <section class="tables" id="rentals">
+<?php
+    echo('<h2>Rentals</h2>');
+
+    if ( isset($_SESSION['error']) ) {
+            echo '<p style="color:red">'.$_SESSION['error']."</p>\n";
+            unset($_SESSION['error']);
+        }
+    if ( isset($_SESSION['success']) ) {
+        echo '<p style="color:green">'.$_SESSION['success']."</p>\n";
+        unset($_SESSION['success']);
+    }
+        echo('<table border="1">'."\n");
+            echo("<tr><th>");
+            echo('Due Date');
+            echo("</th><th>");
+            echo('Return Date');
+            echo("</th><th>");
+            echo('Book');
+            echo("</th><th>");
+            echo('Action');
+            echo("</th></tr>\n");
+            $stmt = $pdo->query(
+                "SELECT r.Due_Date AS Date, 
+                r.Return_Date AS rDate, 
+                concat(a.First_Name, ' ', a.Last_Name) AS Name,
+                a.Account_ID as Account,
+                b.Title AS Title, 
+                b.Book_ID as Book
+                FROM rentals r
+                JOIN accounts a
+                    ON r.Account_FK = a.Account_ID
+                JOIN books b
+                    ON r.Book_FK = b.Book_ID
+                WHERE Account_ID = $account
+                ORDER BY Date");
+            
+
+            while ( $row = $stmt->fetch(PDO::FETCH_ASSOC) ) {
+                echo("<tr><td>");
+                echo(htmlentities($row['Date']));
+                echo("</td><td>");
+                echo(htmlentities($row['rDate']));
+                echo("</td><td>");
+                echo(htmlentities($row['Title']));
+                echo("</td><td>");
+                echo('<form method="post">
+                <input type="hidden" name="Account" value='.$row['Account'].'>
+                <input type="hidden" name="Book" value='.$row['Book'].'>
+                <input type="hidden" name="Title value='.$row['Title'].'>
+                <input type="hidden" name="rDate" value='.$row['rDate'].'>
+                <p class="buttons">
+                  <input type="submit" value="Return" name="return">
+                </p>  
+                </form>');
+                echo("</td></tr>\n");
+            }
+        echo("</table>");
+?>
+</section>
+
 </body>
 </html>
